ff958507e80cd8ef92b4e408cc867c2dc8a91218
Julia <julia_katarina.cohen@hotmail.com>
2021-06-02 13:10:38 +0200

    date formatting

--- server/src/endpoints/get-reviews.ts
+++ server/src/endpoints/get-reviews.ts
@@ -1,20 +1,23 @@
 import { RequestHandler } from "express";
+import { formatISO } from "date-fns";
 import { connection } from "../utils/connection";
 
 export const getReviews: RequestHandler = (req, res) => {
   connection.query("SELECT * from reviews", (err, { rows }) => {
     connection.end();
-    rows.map((row) => ({
+    const formattedRows = rows.map((row) => ({
       id: row.id,
       address: row.address,
       coordinate: row.coordinate,
-      created: row.created,
+      created: formatISO(new Date(row.created), {
+        representation: "date",
+      }),
       quality_ranking: row.quality_rating,
       service_rating: row.service_rating,
       atmosphere_rating: row.atmosphere_rating,
       value_rating: row.value_rating,
       free_text: row.free_text,
     }));
-    res.json(rows);
+    res.json(formattedRows);
   });
 };
