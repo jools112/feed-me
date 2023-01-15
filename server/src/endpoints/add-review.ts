fd5822fa94a69471c6d9affab360d8cc901b3e94
Julia <julia_katarina.cohen@hotmail.com>
2023-01-15 20:43:06 +0100

    apply suggested changes, fix eslint config

--- server/src/endpoints/add-review.ts
+++ server/src/endpoints/add-review.ts
@@ -1,10 +1,8 @@
 import { RequestHandler } from "express";
-import { formatISO } from "date-fns";
-import { ReviewModel } from "@shared/models/review";
-import { ReviewEntity } from "@root/entities/review";
+import { Coordinate } from "@shared/models/review";
 import { dbClient } from "../utils/client";
 
-const randomizeCoords = (centre: number) => {
+const randomizeCoords = (centre: Coordinate) => {
   const randX = Math.random() > 0.5 ? Math.random() : -Math.random();
   const randY = Math.random() > 0.5 ? Math.random() : -Math.random();
   return { x: centre.x + 0.1 * randX, y: centre.y + 0.1 * randY };
@@ -14,7 +12,6 @@ const between = (num: number, min: number, max: number) =>
   num >= min && num <= max;
 
 export const addReview: RequestHandler = (req, response) => {
-  console.log("request: ", req.body);
   const centre = { x: 59.331899, y: 18.060949 };
   const {
     name,
@@ -40,23 +37,17 @@ export const addReview: RequestHandler = (req, response) => {
     typeof freeText === "string";
 
   if (isValid) {
-    console.log(between(name.length, 1, 40));
     dbClient
       .query(
         `INSERT INTO reviews (name, address, coordinate, quality_rating, service_rating, atmosphere_rating, value_rating, free_text) VALUES ('${name}', '${address}', point(${randomCoordinate.x},${randomCoordinate.y}), ${qualityRating}, ${serviceRating}, ${atmosphereRating}, ${valueRating},  '${freeText}');`
       )
-      .then((res) => {
-        response.status(201);
+      .then(() => {
+        response.status(201).send();
       })
-      .catch((err) => {
-        console.error(err);
-      })
-      .finally(() => {
-        // client.end();
+      .catch(() => {
+        response.status(500).send();
       });
   } else {
-    console.log("validation failed!");
-
-    response.status(500).send();
+    response.status(400).send();
   }
 };
